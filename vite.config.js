71459a14b563d6c147fb0d4d6985bc27eb65f5c1
Jhonatan Mejias <Jhonatanmejias@gmail.com>
2024-12-04 20:56:25 -0300

    base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:'https://jhonatalex.github.io/dog-app/'
+  //base:'https://jhonatalex.github.io/dog-app'
 })
